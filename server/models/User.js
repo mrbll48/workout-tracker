79fccdbddb0d72868a2e248d4bbb4de25972f78c
William Marks <william.marks87@gmail.com>
2023-08-16 12:53:00 -0700

    started creating User model, linked to index.js in models directory

---
+++ server/models/User.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// basic structure of User model
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  friends: [userSchema],
+});
+
+// TODO: hash password
+
+// TODO: validate input password for logging in
+
+// TODO: virtuals?
+
+const User = model("User", userSchema);
+
+module.exports = User;
