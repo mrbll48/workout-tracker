687eb1798f069581624287167933b2e8334f18e6
Jair Gomez <jairherber505@gmail.com>
2023-08-19 15:47:21 -0700

    LandingPAge should have an img background

---
+++ client/src/components/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import landing from "../../images/landing.jpg";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <img className="fullscreen-image" src={landing} alt="bg" />
+      <h1>Hey</h1>
+    </div>
+  );
+}
