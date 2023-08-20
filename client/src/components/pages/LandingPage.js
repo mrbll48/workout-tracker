954b3abd18a3af95ccae1d82d92d5ac8023ed95d
Jair Gomez <jairherber505@gmail.com>
2023-08-19 23:53:08 -0700

    working on the login/create account form

--- client/src/components/pages/LandingPage.js
+++ client/src/components/pages/LandingPage.js
@@ -7,19 +7,39 @@ export default function LandingPage() {
       <img className="fullscreen-image" src={landing} alt="nice" />
       <div className="btn-div">
         <button className="button">Login</button>
-        {/* <button className="button">Create Account</button> */}
+        <button className="button">Create Account</button>
       </div>
       <div>
         <h1 className="slogan">
           Get Fit, Share Inspire: <br></br>Uniting Workouts, One Post at a Time
         </h1>
       </div>
+      
       {/* Not working property, if want to test delete/remove the style style={{ visibility: "hidden" }} */}
       <div id="small-device-btn" style={{ visibility: "hidden" }}>
         <button className="button">Login</button>
+        <button className="button">Create Account</button>
       </div>
+
       {/* Create account form */}
-      <div id="form-container" style={{ display: "none" }}></div>
+      <div id="form-container" style={{ display: "none" }}>
+        <form className="form">
+          <p>Login</p>
+          <div className="group">
+            <input required className="main-input" type="text" />
+            <span className="highlight-span"></span>
+            <label className="lebal-email">Email</label>
+          </div>
+          <div className="container-1">
+            <div className="group">
+              <input required className="main-input" type="password" />
+              <span className="highlight-span"></span>
+              <label className="lebal-email">Password</label>
+            </div>
+          </div>
+          <button className="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
