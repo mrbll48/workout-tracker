dd2aefaec84f1ae441a766dbc84404e3d3a6193f
Jair Gomez <jairherber505@gmail.com>
2023-08-22 15:44:03 -0700

    main page

--- client/src/components/pages/MainPage.js
+++ client/src/components/pages/MainPage.js
@@ -1,10 +1,20 @@
 import React from "react";
-import "../css/mainpage.css"
+import "../css/mainpage.css";
 
-export default function MainPage ()  {
+import { IconContext } from "react-icons";
+import { FaUserCircle } from "react-icons/fa";
+
+export default function MainPage() {
   return (
-    <div className="main-container">MainPage
-    <div></div>
+    <div className="main-container">
+      <div className="nav-icons">
+        <IconContext.Provider value={{ color: "white", size: "2.5em" }}>
+          <div>
+            <FaUserCircle />
+          </div>
+        </IconContext.Provider>
+        ;
+      </div>
     </div>
-  )
+  );
 }
