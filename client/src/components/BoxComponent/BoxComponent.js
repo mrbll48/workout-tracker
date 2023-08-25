8818e2670eb39ffd5e9cc6344788929e49603566
Jair Gomez <jairherber505@gmail.com>
2023-08-24 21:40:31 -0700

    api request works

--- client/src/components/BoxComponent/BoxComponent.js
+++ client/src/components/BoxComponent/BoxComponent.js
@@ -1,9 +1,41 @@
 import React from "react";
 import "../css/box-component.css";
 
+// import axios from "axios";
 import Dropdown from "react-bootstrap/Dropdown";
+const workoutAPIKey = "4Z7299Xd9HEZMOuF2j15sg==HS0gwsLVKjmqzWlK";
 
-function BoxComponent({ image, dropdownText }) {
+function BoxComponent({ image, workoutType, workoutMuscle }) {
+  // const getWorkout = () => {
+  //   axios
+  //     .get(
+  //       `https://api.api-ninjas.com/v1/exercises?type=${workoutType}&muscle=${workoutMuscle}&difficulty=beginner`
+  //     )
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
+  function searchApi(workoutMuscle, workoutDifficulty) {
+    const options = {
+      method: "GET",
+      headers: { "x-api-key": workoutAPIKey },
+    };
+    console.log(workoutType);
+    let url = `https://api.api-ninjas.com/v1/exercises?type=${workoutType}&muscle=chest&difficulty=beginner`;
+    // fetch request for workouts
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((workouts) => console.log(workouts))
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // "https://api.quotable.io/random"()
   return (
     <div className="container">
       <div className="img-container">
@@ -17,10 +49,10 @@ function BoxComponent({ image, dropdownText }) {
               }}
               id="dropdown-basic"
             >
-              {dropdownText}
+              {workoutType}
             </Dropdown.Toggle>
             <Dropdown.Menu>
-              <Dropdown.Item>Action</Dropdown.Item>
+              <Dropdown.Item onClick={searchApi}>Action</Dropdown.Item>
               <Dropdown.Item>Another action</Dropdown.Item>
               <Dropdown.Item>Something else</Dropdown.Item>
             </Dropdown.Menu>
