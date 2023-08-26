234f1680fb65602728be974908efc82527ad7149
William Marks <william.marks87@gmail.com>
2023-08-25 21:52:04 -0700

    login route working on front end.

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,60 +1,55 @@
 import React, { useState } from "react";
 import "./css/landingpage.css";
-// import { ADD_USER } from "../../utils/mutations";
-// import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
 import Auth from "../utils/auth";
 
 // export default function LandingPage() {
 
 const SignupForm = () => {
-  const [loginForm, setLoginForm] = useState(false);
-  const [createFrom, setCreateForm] = useState(false);
   // set initial form state
   const [userFormData, setUserFormData] = useState({
     username: "",
     email: "",
     password: "",
   });
+  const [addUser, { error, data }] = useMutation(ADD_USER);
   // set state for form validation
-  const [validated] = useState(false);
-  // set state for alert
-  const [showAlert, setShowAlert] = useState(false);
-  // const [addUser, { error }] = useMutation(ADD_USER);
+  // const [validated] = useState(false);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
+    console.log({ ...userFormData });
     setUserFormData({ ...userFormData, [name]: value });
   };
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
     // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
-    // try {
-    //   console.log(userFormData);
-    //   // const response = await createUser(userFormData);
-    //   const { data } = await addUser({
-    //     variables: { ...userFormData },
-    //   });
-
-    //   console.log(data);
-
-    //   Auth.login(data.addUser.token);
-    // } catch (error) {
-    //   console.error(error);
-    //   setShowAlert(true);
+    event.preventDefault();
+    // const form = event.currentTarget;
+    // if (form.checkValidity() === false) {
+    //   event.stopPropagation();
     // }
 
-    setUserFormData({
-      username: "",
-      email: "",
-      password: "",
-    });
+    try {
+      console.log(userFormData);
+      // const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
+      console.log(data);
+
+      Auth.login(data.addUser.token);
+    } catch (error) {
+      console.error(error);
+    }
+
+    // setUserFormData({
+    //   username: "",
+    //   email: "",
+    //   password: "",
+    // });
   };
   return (
     <div>
@@ -95,29 +90,52 @@ const SignupForm = () => {
         )} */}
 
       <div id="form-container">
-        <form className="form">
+        <form className="form" onSubmit={handleFormSubmit}>
           <p>Create Account</p>
           <div className="group">
-            <input required className="main-input" type="text" />
+            <input
+              required
+              className="main-input"
+              name="username"
+              type="text"
+              value={userFormData.username}
+              onChange={handleInputChange}
+            />
             <span className="highlight-span"></span>
             <label className="lebal-email">Username</label>
           </div>
 
           <div className="container-1">
             <div className="group">
-              <input required className="main-input" type="password" />
+              <input
+                required
+                className="main-input"
+                name="email"
+                type="email"
+                onChange={handleInputChange}
+                value={userFormData.email}
+              />
               <span className="highlight-span"></span>
               <label className="lebal-email">Email</label>
             </div>
           </div>
           <div className="container-1">
             <div className="group">
-              <input required className="main-input" type="password" />
+              <input
+                required
+                className="main-input"
+                name="password"
+                type="password"
+                onChange={handleInputChange}
+                value={userFormData.password}
+              />
               <span className="highlight-span"></span>
               <label className="lebal-email">Password</label>
             </div>
           </div>
-          <button className="submit">Submit</button>
+          <button className="submit" type="submit">
+            Submit
+          </button>
         </form>
       </div>
     </div>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $password: String!, $email: Sting!) {
+  mutation addUser($username: String!, $password: String!, $email: String!) {
     addUser(username: $username, password: $password, email: $email) {
       token
       user {
