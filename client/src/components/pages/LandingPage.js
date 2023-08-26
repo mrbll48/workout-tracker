ef4fca63de44f0fa0cd8f85e52cc64b30797c256
William Marks <william.marks87@gmail.com>
2023-08-26 07:28:58 -0700

    added logout functionality to LandingPage.js

--- client/src/components/pages/LandingPage.js
+++ client/src/components/pages/LandingPage.js
@@ -1,35 +1,61 @@
 import { useState } from "react";
 import "../css/landingpage.css";
 
+import Auth from "../../utils/auth";
+
 import { Button, Modal } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SignupForm from "../SignupForm";
 import LoginForm from "../LoginForm";
 
 export default function LandingPage() {
+  // function to import logout from auth and assign it to logout variable
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   const [showLogin, setShowLogin] = useState(false);
   const [showCreate, setShowCreate] = useState(false);
   const handleCloseLogin = () => setShowLogin(false);
   const handleShowLogin = () => {
-    setShowLogin(true)
-    setShowCreate(false)
-  }
+    setShowLogin(true);
+    setShowCreate(false);
+  };
   const handleCloseCreate = () => setShowCreate(false);
   const handleShowCreate = () => {
-    setShowCreate(true)
-    setShowLogin(false)
-  }
+    setShowCreate(true);
+    setShowLogin(false);
+  };
   return (
     <div className="bg-img ">
       {/* <img className="" src={landing} alt="nice" /> */}
       <div className="btn-div">
-        <button className="button" onClick={handleShowLogin}>
+        {/* added in logic for displaying logout button */}
+        {Auth.loggedIn() ? (
+          <>
+            <span>Hey there, {Auth.getProfile().data.username}!</span>
+            <button className="button" onClick={logout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <button className="button" onClick={handleShowLogin}>
+              Login
+            </button>
+            <button className="button" onClick={handleShowCreate}>
+              Create Account
+            </button>
+          </>
+        )}
+
+        {/* <button className="button" onClick={handleShowLogin}>
           Login
-          </button>
+        </button>
         <button className="button" onClick={handleShowCreate}>
           Create Account
-        </button>
-
+        </button> */}
       </div>
       {showLogin ? (
         <LoginForm />
@@ -38,13 +64,7 @@ export default function LandingPage() {
           Get Fit, Share Inspire: <br></br>Uniting Workouts, One Post at a Time
         </h1>
       )}
-      {showCreate ? (
-        <SignupForm />
-      ) : (
-       null
-      )}
+      {showCreate ? <SignupForm /> : null}
     </div>
   );
 }
-
-
