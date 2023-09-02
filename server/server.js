72dce12eb3a844c24a9fedf21a0cd7993f4db5cf
mrbll48 <william.marks87@gmail.com>
2023-09-01 19:10:58 -0700

    STILL DEBUGGING

--- server/server.js
+++ server/server.js
@@ -25,6 +25,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 app.use("/images", express.static(path.join(__dirname, "../client/images")));
 
 const startApolloServer = async () => {
